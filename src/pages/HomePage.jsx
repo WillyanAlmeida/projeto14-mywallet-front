31b919cf3f23ad5870d16ec4d5e7e514af6ea971
Willyan <wkinhaamaral@gmail.com>
2023-07-11 13:39:49 -0300

    feat/ logout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,6 +17,7 @@ export default function HomePage() {
   }
 
   const navigate = useNavigate()
+  if(!user) navigate("/")
   useEffect(() => {
     const requisicao = axios.get(`${import.meta.env.VITE_API_URL}/home`, config);
 
